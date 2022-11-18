a429a057aab1d11a45049a658bcb362403afecb0
Sergio j Silva <juniosergio034@gmail.com>
2022-11-18 11:12:20 -0300

    atualizado

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 function adicionar(){
-const input = document.querySelector("input").value;
+const input = document.querySelector("#add").value;
 const ul = document.querySelector("ul");
 var li = document.createElement('li');
 
@@ -9,15 +9,26 @@ var li = document.createElement('li');
     var deletar = document.createElement('button');
     deletar.setAttribute("class", "fa-solid fa-delete-left");
 
-    ul.appendChild(li)
-    ul.appendChild(deletar)
-
     deletar.addEventListener("click", function(){
         li.remove();
         deletar.remove();
+        editar.remove();
     })
     
-  
+    var editar = document.createElement('button');
+    editar.setAttribute("class", "fa-solid fa-pen-to-square");
+
+      editar.addEventListener("click", ()=>{
+        let atualizarInput = li.innerHTML;
+        let atualizar = document.getElementById("edit").value = atualizarInput
+      })
+
+
+
+    ul.appendChild(li)
+    ul.appendChild(deletar)
+    ul.appendChild(editar)
+
 }
 
 
